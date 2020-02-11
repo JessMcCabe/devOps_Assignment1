11cfd3ca6ae346a51062cdb5da90259c7227ec03
Jessica McCabe <mccabejessica88@gmail.com>
2020-02-11 19:21:01 +0000

    Update terminate_instances.py
    Correct if else

--- terminate_instances.py
+++ terminate_instances.py
@@ -2,11 +2,11 @@
 import sys
 import boto3
 ec2 = boto3.resource('ec2')
-if sys.argv[1:] True
-    for instance_id in sys.argv[1:]:
+if sys.argv[1:] is None:
+  print("You did not provide an instance id, please provide the id of the instance you wish to terminate")
+else:
+  for instance_id in sys.argv[1:]:
         instance = ec2.Instance(instance_id)
         response = instance.terminate()
         print (response)
-else 
-  print("You did not provide an instance id, please provide the id of the instance you wish to terminate")
   quit()
